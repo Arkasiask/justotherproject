57910e757f02ee031091fa800fdc86326593c1cb
Arkasiask <46874535+Arkasiask@users.noreply.github.com>
2019-04-19 16:19:37 -0400

    Add files via upload

--- comandos/ascii.js
+++ comandos/ascii.js
@@ -3,14 +3,19 @@ const bot = new Discord.Client();
 const ascii = require("ascii-art")
 
 exports.run = (bot,message,args) => {
+	 
 	if(!args.join(' ')) return message.channel.send("¿Que quieres convertir en ascii?")    
 
-		ascii.font(args.join(' '), 'Doom', async txt => {
-			message.channel.send(txt,{
+		ascii.font(args.join(' '), 'Doom', function(rendered) {
+			rendered = rendered.trimRight();
+
+			if (rendered.length > 1000) return message.channel.send('Ese mensaje es muy largo');
+
+			message.channel.send(rendered, {
 				code: 'md'
 			});
-		});
 
+		})
 }
 
 exports.help = {
