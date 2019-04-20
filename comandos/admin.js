cac97897f9cd31d498a6d679c5b3b386add507b3
Arkasiask <46874535+Arkasiask@users.noreply.github.com>
2019-04-20 09:54:08 -0400

    Add files via upload

--- comandos/admin.js
+++ comandos/admin.js
@@ -7,7 +7,7 @@ exports.run = (bot,message,args) => {
         .setTitle(":hammer_pick: | Lista de comandos administrativos para Moderadores y Owners")
         .setColor("#e5106c")
         .setDescription('Nota: Para poder hacer uso de estos comandos debe existir un canal log llamado ``#klog`` donde se guardará el registro de cada acción con estos comandos')
-        .addField('•``k!say``', 'Envia un mensaje en nombre de **Kasia** en el canal actual o un canal especifico | ``k!say #canal <mensaje>')
+        .addField('•``k!say``', 'Envia un mensaje en nombre de **Kasia** en el canal actual o un canal especifico | ``k!say #canal <mensaje>``')
         .addField('•``k!cprefix``', 'Cambia el prefix de **Kasia** por uno personalizado | ``k!cprefix <nuevo prefix>``')
         .addField('•``k!clear``', 'Borra una cantidad especifica de mensajes | ``k!clear #``')
         .addField('•``k!kick``', 'Expulsa a un usuario del servidor | ``k!kick @user <razon>``')
