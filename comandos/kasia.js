58742d0226297fc0190d3a143ab16f56fa8a2001
Arkasiask <46874535+Arkasiask@users.noreply.github.com>
2019-06-04 00:59:16 -0400

    Add files via upload

--- comandos/kasia.js
+++ comandos/kasia.js
@@ -3,21 +3,24 @@ const bot = new Discord.Client();
 
 exports.run = (bot,message,args) => {
    let bicon = message.client.iconURL;
+   let gsize = bot.guilds.size
+   let usize = bot.users.size
     let embed = new Discord.RichEmbed()
-      .setTitle(":heart: HOLA! Soy Kasia")
+      .setTitle(" HOLA! Soy Kasia :heart:")
       .setColor("#cef442")
-      .setFooter("Kasia | Tabla de información sobre el bot", bot.user.avatarURL)
-      .setDescription('**Conoce un poco más de mi**')
+      .setFooter("Kasia | Información básica sobre mi!!! 😀 ", bot.user.avatarURL)
+      .setDescription('Soy producto de la idea de crear un bot exclusivo para un solo servidor, algo nada ambicioso, más sin embargo, por la buena recepción que tuvo por parte de los usuarios se comenzó a trabajar en la expansión de mi a otros servidores. Mi desarrollo cuenta con la integración de comandos de casi todo tipo, más que todo esos famosos comandos **interactivos** entre usuarios, incluyendo también algunos básicos de **utilidad** y otros **administrativos.** ')
       .setThumbnail(bot.user.avatarURL)
       .addField("• Mi Nombre:", bot.user.username, true)
       .setTimestamp()
       .addField("• Programador:", "Arkasia#7701", true)
-      .addField("• Nº de comandos:", '``42``', true)
-      .addField("• Nº de Servers:", bot.guilds.size, true)
-      .addField("• Nº de Usuarios", bot.users.size, true)
+      .addField("• Nº de comandos:", '``40``', true)
+      .addField("• Nº de Servers:", " ``"+gsize+"`` ", true)
+      .addField("• Nº de Usuarios", " ``"+usize+"`` ", true)
       .addField("• Fecha de mi creación:", bot.user.createdAt, true)
-      .addField(" 📥 Puedes invitarme a tu servidor con el comando", '``k!invite``', true)
-      .addField(":globe_with_meridians: Pagina:", "https://kasia-bot.webnode.com.ve")
+      .addField("📥 Puedes invitarme a tu servidor con el comando", '``k!invite``', true)
+      .addField("🌐 Pagina:", "https://kasia-bot.webnode.com.ve")
+      .addField("💎 Patreon:", "https://www.patreon.com/kasiabot")
 
       message.channel.send(embed)
 
